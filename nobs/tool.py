49b3e6bcb8d4faa01da27338d947c4e080ac41a8
John Bachan <jdbachan@lbl.gov>
2017-08-09 13:09:08 -0700

    nobs compatibility fix for python2/3

--- nobs/tool.py
+++ nobs/tool.py
@@ -1,4 +1,12 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
+
+import sys
+
+assert sys.version_info >= (2,6)
+
+if sys.version_info[0] != 2:
+  import os
+  os.execvp('python2', sys.argv)
 
 from nobs.tool_main import main
 
