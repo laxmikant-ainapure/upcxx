d6bdb5d25533f55a70a8a4b2a23c7c6d7aef7f88
bonachea <dobonachea@lbl.gov>
2017-09-04 22:35:49 -0400

    use make --no-print-directory in makefile_extract
    which appears to be more reliable than 'make -s'

--- nobsrule.py
+++ nobsrule.py
@@ -955,10 +955,10 @@ def env(name, otherwise):
 def makefile_extract(makefile, varname):
   """
   Extract a variable's value from a makefile.
-  -s (or --no-print-directory) is required to ensure correct behavior when nobs was invoked by make
+  --no-print-directory is required to ensure correct behavior when nobs was invoked by make
   """
   import subprocess as sp
-  p = sp.Popen(['make','-s','-f','-','gimme'], stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE)
+  p = sp.Popen(['make','--no-print-directory','-f','-','gimme'], stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE)
   tmp = ('include {0}\n' + 'gimme:\n' + '\t@echo $({1})\n').format(makefile, varname)
   val, _ = p.communicate(tmp)
   if p.returncode != 0:
