3693a50ddee64174fe080d45bba26026b2e0da62
Scott B. Baden <baden@svamp.dhcp.lbl.gov>
2017-09-06 21:12:40 -0700

    fixed indexing errors

--- example/rb1d/upcxx/fillGhost.cpp
+++ example/rb1d/upcxx/fillGhost.cpp
@@ -13,18 +13,18 @@ void fillGhost(double *u, int hi, int lo, global_ptr<double> &uL, global_ptr<dou
   intrank_t nranks = upcxx::rank_n();
   future<double> fL, fR;
   if ( !(uL.is_null())){
-      fL = rget(uL);                                    // Equivalent to future<> fL = rget(uL,&u0,1)
+      fL = rget(uL+hi-lo-1);             // Equivalent to future<> fL = rget(uL,&u0,1)
 //      cout << "Left Get on " << myrank << " initiates\n" ;
       double u0 = wait(fL);
-      cout << "Got " << u0 << " from Left\n";
+//      cout << "Got " << u0 << " from Left\n";
 //      cout << "Left Get on " << myrank << " completes\n" ;
       u[0] = u0;
   }
   if ( !(uR.is_null())){
-      fR = rget(uR);
+      fR = rget(uR+1);
 //      cout << "Right Get on " << myrank << " initiates\n" ;
       double u1 = wait(fR);
-      cout << "Got " << u1 << " from Right\n";
+//      cout << "Got " << u1 << " from Right\n";
 //      cout << "Right Get on " << myrank << " completes\n" ;
       u[hi-lo] = u1;
   }
