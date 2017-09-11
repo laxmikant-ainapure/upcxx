d9e1ef349ad0f97a44cdd23e1260dd208fefe7ef
Scott Baden <sbaden@ucsd.edu>
2017-09-10 22:42:27 -0700

    added some output for debugging

--- example/rb1d/upcxx/fillGhost.cpp
+++ example/rb1d/upcxx/fillGhost.cpp
@@ -11,21 +11,20 @@ void fillGhost(double *u, int hi, int lo, global_ptr<double> &uL, global_ptr<dou
 {
   intrank_t myrank = upcxx::rank_me();
   intrank_t nranks = upcxx::rank_n();
-  future<double> fL, fR;
   if ( !(uL.is_null())){
-      fL = rget(uL+hi-lo-1);             // Equivalent to future<> fL = rget(uL,&u0,1)
-//      cout << "Left Get on " << myrank << " initiates\n" ;
+      // Equivalent to future<> fL = rget(uL,&u0,1)
+      
+      printf("Left ptr: %p [%d]\n",uL,myrank);
+      future<double> fL = rget(uL+hi-lo-1);  
       double u0 = wait(fL);
 //      cout << "Got " << u0 << " from Left\n";
-//      cout << "Left Get on " << myrank << " completes\n" ;
       u[0] = u0;
   }
   if ( !(uR.is_null())){
-      fR = rget(uR+1);
-//      cout << "Right Get on " << myrank << " initiates\n" ;
+      future<double> fR = rget(uR+1);
+      printf("Right ptr: %p [%d]\n",uR,myrank);
       double u1 = wait(fR);
 //      cout << "Got " << u1 << " from Right\n";
-//      cout << "Right Get on " << myrank << " completes\n" ;
       u[hi-lo] = u1;
   }
 }
