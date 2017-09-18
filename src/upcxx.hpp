c99918b6589fae7d79fba66241c9a1484d483558
Steven Hofmeyr <shofmeyr@lbl.gov>
2017-09-18 11:33:36 -0700

    added version id to upcxx.hpp

--- src/upcxx.hpp
+++ src/upcxx.hpp
@@ -1,6 +1,8 @@
 #ifndef _a6becf70_cd32_4da7_82e9_379ce395b781
 #define _a6becf70_cd32_4da7_82e9_379ce395b781
 
+#define UPCXX_VERSION 1.0
+
 #include <upcxx/allocate.hpp>
 #include <upcxx/backend.hpp>
 #include <upcxx/dist_object.hpp>
