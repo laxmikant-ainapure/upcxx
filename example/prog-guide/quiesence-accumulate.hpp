1568fd8a3e6f7c00e013a95241a86788fd75df1f
bonachea <dobonachea@lbl.gov>
2017-09-18 13:10:23 -0400

    prog-guide: update example code

--- example/prog-guide/quiesence-accumulate.hpp
+++ example/prog-guide/quiesence-accumulate.hpp
@@ -6,13 +6,13 @@ int accumulate(int my_hits)
 {
     // cannot wait for the RPC - there is no return
     upcxx::rpc_ff(0, [](int my_hits) { hits += my_hits; n_done++; }, my_hits);
-    // wait until all ranks have fired off the RPCs
-    upcxx::barrier();
     if (upcxx::rank_me() == 0) {
-        // spin waiting for all ranks to complete RPCs
+        // spin waiting for RPCs from all ranks to complete
         // When spinning, call the progress function to 
         // ensure rank 0 processes waiting RPCs
         while (n_done != upcxx::rank_n()) upcxx::progress();
     }
+    // wait until all RPCs have been processed (quiescence)
+    upcxx::barrier();
     return hits;
 }
