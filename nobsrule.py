0f5e44e5b5d5c5377d970e478d634f7a043a8253
bonachea <dobonachea@lbl.gov>
2017-09-23 12:08:21 -0700

    configure EX with --disable-parsync
    UPCXX is not using parsync, so this reduces build/install time/space.

--- nobsrule.py
+++ nobsrule.py
@@ -885,6 +885,8 @@ class gasnet_configured:
         '--disable-psm','--disable-mxm','--disable-portals4','--disable-ofi',
         # avoid a known issue with -Wnested-externs, until it gets a proper fix in EX
         '--disable-dev-warnings',
+        # disable the parsync mode which is not used by UPCXX
+        '--disable-parsync',
       ]
       
       print>>sys.stderr, 'Configuring GASNet...'
