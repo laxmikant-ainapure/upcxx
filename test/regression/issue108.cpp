6197fd722cd6a5c712508ed383572c0f79fe307f
bonachea <dobonachea@lbl.gov>
2018-01-16 00:26:10 -0500

    Fix regression/issue108 output
    Make the test self-verifying with output amenable to CI

--- test/regression/issue108.cpp
+++ test/regression/issue108.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <upcxx/upcxx.hpp>
 
-void foo0(void) { std::cout << "foo0" << std::endl; }
-void foo2I(int a, int b) { std::cout << "foo2I(" << a << "," << b << ")" << std::endl; }
+#include "../util.hpp"
+
+int calls = 0;
+
+void foo0(void) { calls++; std::cout << "foo0" << std::endl; }
+void foo2I(int a, int b) { calls++; std::cout << "foo2I(" << a << "," << b << ")" << std::endl; }
 
 int main(int argc, char *argv[])
 {
     upcxx::init();
+    print_test_header();
 
     foo0();    // works (regular C++)
     (&foo0)(); // works (regular C++)
@@ -15,11 +20,10 @@ int main(int argc, char *argv[])
 
     foo2I(1,2);    // works (regular C++)
     (&foo2I)(1,2); // works (regular C++)
-    #if 1
     upcxx::rpc(upcxx::rank_me(),foo2I,1,2).wait();  // FAILS!!!
-    #endif
     upcxx::rpc(upcxx::rank_me(),&foo2I,1,2).wait(); // works
 
+    print_test_success(calls == 8);
     upcxx::finalize();
     return 0;
 }   
