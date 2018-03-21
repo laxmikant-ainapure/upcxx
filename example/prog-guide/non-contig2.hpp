343ecea408a398464f7f5934425f8551879d0ce0
Brian VS <bvstraalen@lbl.gov>
2018-03-21 13:24:03 -0700

    C arrays for example instead of std::vector

--- example/prog-guide/non-contig2.hpp
+++ example/prog-guide/non-contig2.hpp
@@ -1,4 +1,4 @@
-vector<pair<particle_t*, size_t>> src({{{srcP+12, 22},{srcP+66,12},{srcP,4}}});
-vector<pair<global_ptr<particle_t>, size_t>> dest({{{destP,38}}});
-auto f1 = rput_irregular(src.begin(), src.end(), dest.begin(), dest.end());
-f1.wait();
+pair<particle_t*, size_t> src[]={{srcP+12, 22},{srcP+66,12},{srcP,4}};
+pair<global_ptr<particle_t>, size_t> dest[]={{destP,38}};
+auto f = rput_irregular(begin(src), end(src), begin(dest), end(dest));
+f.wait();
