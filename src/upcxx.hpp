3f517abd38718ad870c678b36a3162c4cc61602b
bonachea <dobonachea@lbl.gov>
2018-03-26 17:02:33 -0700

    update version identification in preparation for release

--- src/upcxx.hpp
+++ src/upcxx.hpp
@@ -3,7 +3,7 @@
 
 // UPCXX_VERSION release identifier format:
 // YYYYMMPP = [YEAR][MONTH][PATCH]
-#define UPCXX_VERSION 20180101
+#define UPCXX_VERSION 20180300
 
 #include <upcxx/allocate.hpp>
 #include <upcxx/allreduce.hpp>
