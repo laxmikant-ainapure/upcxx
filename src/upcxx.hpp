2a0f30d83ce8cdade964f55b3e275e45953d3a02
bonachea <dobonachea@lbl.gov>
2018-03-28 20:34:51 -0400

    post-release version bump

--- src/upcxx.hpp
+++ src/upcxx.hpp
@@ -3,7 +3,7 @@
 
 // UPCXX_VERSION release identifier format:
 // YYYYMMPP = [YEAR][MONTH][PATCH]
-#define UPCXX_VERSION 20180300
+#define UPCXX_VERSION 20180301
 
 #include <upcxx/allocate.hpp>
 #include <upcxx/allreduce.hpp>
