e1306526d70912958f8ffa2383bbbecaa2161aee
mathias jacquelin <mathias.jacquelin@gmail.com>
2018-09-19 19:23:11 -0700

    fixing the code thanks to Dan and Paul's comments

--- example/prog-guide/persona-example-rputs.cpp
+++ example/prog-guide/persona-example-rputs.cpp
@@ -13,29 +13,29 @@
 using namespace std; 
 
 int main () {
-	upcxx::init(); 
-	
+  upcxx::init(); 
+  
   //SNIPPET
-	const int n = upcxx::rank_n();
-	const int me = upcxx::rank_me();
+  const int n = upcxx::rank_n();
+  const int me = upcxx::rank_me();
 
   //create landing zones  
 
-	vector<upcxx::global_ptr<int>> ptrs(n); 
-	ptrs[me] = upcxx::new_array<int>(n);
+  vector<upcxx::global_ptr<int>> ptrs(n); 
+  ptrs[me] = upcxx::new_array<int>(n);
 
   //share with other processes
-	for(int i=0; i < n; i++)
-		ptrs[i] = upcxx::broadcast(ptrs[i], i).wait(); 
-	
+  for(int i=0; i < n; i++)
+    ptrs[i] = upcxx::broadcast(ptrs[i], i).wait(); 
+  
   //initialize local chunk of the landing zone.
-	int *local = ptrs[me].local(); 
-	for(int i=0; i < n; i++)
-		local[i] = -1;
+  int *local = ptrs[me].local(); 
+  for(int i=0; i < n; i++)
+    local[i] = -1;
 
-	upcxx::barrier(); 
-	
-	
+  upcxx::barrier(); 
+  
+  
   atomic<int> thread_barrier(0);
   int done_count = 0;
 
@@ -46,9 +46,9 @@ int main () {
   thread progress_thread( [&]() {
         //capture the progress_persona
         upcxx::persona_scope scope(progress_persona);
-		    // progress thread drains progress until all work has been performed
-		    while(done_count != n)
-			    upcxx::progress();
+        // progress thread drains progress until all work has been performed
+        while(done_count != n)
+          upcxx::progress();
 
         cout<<"Progress thread on process "<<upcxx::rank_me()<<" is done"<<endl; 
         //unlock the other threads
@@ -58,41 +58,41 @@ int main () {
 
   //create another thread to issue the rputs
   thread submit_thread( [&]() {  
-	  	// launch a rput to each rank
-	  	for (int i = 0; i < n ; i++ ) {
-	  		upcxx::rput(
-	  			&me, ptrs[(me + i)%n] + i, 1,
-	  			// each rput is resolved in continuation on the progress thread
-	  			upcxx::operation_cx::as_lpc(
-	  				progress_persona,
-	  				[&,i]() {
-	  					// fetch the value just put
-	  					upcxx::rget(ptrs[(me + i)%n] + i).then(
-	  						[&](int got) {
-	  							assert(got == me);
-	  							done_count += 1;
-	  						}
-	  					);
-	  				}
-	  			)
-	  		);
-	  	}
-	  	
+      // launch a rput to each rank
+      for (int i = 0; i < n ; i++ ) {
+        upcxx::rput(
+          &me, ptrs[(me + i)%n] + i, 1,
+          // each rput is resolved in continuation on the progress thread
+          upcxx::operation_cx::as_lpc(
+            progress_persona,
+            [&,i]() {
+              // fetch the value just put
+              upcxx::rget(ptrs[(me + i)%n] + i).then(
+                [&](int got) {
+                  assert(got == me);
+                  done_count += 1;
+                }
+              );
+            }
+          )
+        );
+      }
+      
       //block here until the progress thread has executed all lpcs
       while(thread_barrier.load(memory_order_acquire) != 1){
         sched_yield();
         upcxx::progress();
       }
-	  });
+    });
 
   submit_thread.join();
   progress_thread.join();
 
-	upcxx::barrier();
+  upcxx::barrier();
 
   if ( upcxx::rank_me()==0 )
     cout<<"SUCCESS"<<endl;
   //SNIPPET
     
-	upcxx::finalize(); 
+  upcxx::finalize(); 
 }
