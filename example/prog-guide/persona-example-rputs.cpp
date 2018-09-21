485723c40aa626b33fcc237b4bf6c1ba8df55978
mathias jacquelin <mathias.jacquelin@gmail.com>
2018-09-20 21:07:46 -0700

    updated examples from the guide

--- example/prog-guide/persona-example-rputs.cpp
+++ example/prog-guide/persona-example-rputs.cpp
@@ -12,87 +12,57 @@
 
 using namespace std; 
 
+//SNIPPET
 int main () {
   upcxx::init(); 
-  
-  //SNIPPET
-  const int n = upcxx::rank_n();
-  const int me = upcxx::rank_me();
-
-  //create landing zones  
-
-  vector<upcxx::global_ptr<int>> ptrs(n); 
-  ptrs[me] = upcxx::new_array<int>(n);
-
-  //share with other processes
-  for(int i=0; i < n; i++)
-    ptrs[i] = upcxx::broadcast(ptrs[i], i).wait(); 
-  
-  //initialize local chunk of the landing zone.
-  int *local = ptrs[me].local(); 
-  for(int i=0; i < n; i++)
-    local[i] = -1;
-
-  upcxx::barrier(); 
-  
-  
-  atomic<int> thread_barrier(0);
-  int done_count = 0;
-
-  //declare an agreed upon persona for the progress thread
+  // create a landing zone, and share it through a dist_object
+  // allocate and initialize with local rank
+  upcxx::dist_object<upcxx::global_ptr<int>> dptrs(upcxx::new_<int>(upcxx::rank_me()));
+  upcxx::global_ptr<int> my_ptr = *dptrs;
+  // fetch my neighbor's pointer to its landing zone
+  upcxx::intrank_t neigh_rank = (upcxx::rank_me() + 1)%upcxx::rank_n();
+  upcxx::global_ptr<int> neigh_ptr = dptrs.fetch(neigh_rank).wait();
+  // declare an agreed upon persona for the progress thread
   upcxx::persona progress_persona;
-
-  //create the progress thread
+  atomic<int> thread_barrier(0);
+  bool done = false;
+  // create the progress thread
   thread progress_thread( [&]() {
-        //capture the progress_persona
+        // push progress_persona onto this thread's persona stack
         upcxx::persona_scope scope(progress_persona);
-        // progress thread drains progress until all work has been performed
-        while(done_count != n)
+        // progress thread drains progress until work is done
+        while (!done)
           upcxx::progress();
-
         cout<<"Progress thread on process "<<upcxx::rank_me()<<" is done"<<endl; 
         //unlock the other threads
         thread_barrier += 1;
       });
-
-
-  //create another thread to issue the rputs
+  // create another thread to issue the rputs
   thread submit_thread( [&]() {  
-      // launch a rput to each rank
-      for (int i = 0; i < n ; i++ ) {
-        upcxx::rput(
-          &me, ptrs[(me + i)%n] + i, 1,
-          // each rput is resolved in continuation on the progress thread
-          upcxx::operation_cx::as_lpc(
-            progress_persona,
-            [&,i]() {
-              // fetch the value just put
-              upcxx::rget(ptrs[(me + i)%n] + i).then(
-                [&](int got) {
-                  assert(got == me);
-                  done_count += 1;
-                }
-              );
-            }
-          )
-        );
-      }
-      
-      //block here until the progress thread has executed all lpcs
+      // create a completion object to execute a LPC on the progress_thread
+      // which verifies that the value we got was the rank of our neighbor
+      auto cx = upcxx::operation_cx::as_lpc( progress_persona, [&](int got) {
+              assert(got == neigh_rank);
+              //signal that work is complete
+              done = true;
+            });
+      // use this completion object on the rget
+      upcxx::rget(neigh_ptr, cx);
+      // block here until the progress thread has executed all LPCs
       while(thread_barrier.load(memory_order_acquire) != 1){
         sched_yield();
         upcxx::progress();
       }
     });
-
+  // wait until all threads finish their work
   submit_thread.join();
   progress_thread.join();
-
+  // wait until all processes are done
   upcxx::barrier();
-
   if ( upcxx::rank_me()==0 )
     cout<<"SUCCESS"<<endl;
-  //SNIPPET
-    
+  // delete my landing zone 
+  upcxx::delete_(my_ptr);
   upcxx::finalize(); 
 }
+//SNIPPET
--- example/prog-guide/persona-example.cpp
+++ example/prog-guide/persona-example.cpp
@@ -31,54 +31,48 @@ int main(int argc, char *argv[])
   upcxx::barrier();
 
   //SNIPPET
-  // now try to fetch keys inserted by neighbor
+  // try to fetch keys inserted by neighbor
   // note that in this example, keys and values are assumed to be the same
   const int num_threads = 10;
   thread * threads[num_threads];
-
+  // declare an agreed upon persona for the progress thread
   upcxx::persona progress_persona;
   atomic<int> thread_barrier(0);
   int lpc_count = N;
-
-  //liberate the master persona to allow the progress thread to use it
+  // liberate the master persona to allow the progress thread to use it
   upcxx::liberate_master_persona();
-
-  //Create a thread to execute the assertions while lpc_count is greater than 0
+  // create a thread to execute the assertions while lpc_count is greater than 0
   thread progress_thread( [&]() {
-      //capture the master persona
+      // push the master persona onto this thread's stack
       upcxx::persona_scope scope(upcxx::master_persona());
-      //capture the progress_persona as well
+      // push the progress_persona as well
       upcxx::persona_scope progress_scope(progress_persona);
-      // wait until all assertions in lpcs are complete
+      // wait until all assertions in LPCs are complete
       while(lpc_count > 0) { 
         sched_yield();
         upcxx::progress();
       }
-    
       cout<<"Progress thread on process "<<upcxx::rank_me()<<" is done"<<endl; 
-      //unlock the other threads
+      // unlock the other threads
       thread_barrier += 1;
       });
-
-
-  //Launch multiple threads performing the find operation
+  // launch multiple threads to perform find operations
   for (int tid=0; tid<num_threads; tid++) {
     threads[tid] = new thread( [&,tid] () {
-        //split the work across threads
+        // split the work across threads
         long num_asserts = N / num_threads;
         long i_beg = tid * num_asserts;
         long i_end = tid==num_threads-1?N:(tid+1)*num_asserts;
         for (long i = i_beg; i < i_end; i++) {
           string key = to_string((upcxx::rank_me() + 1) % upcxx::rank_n()) + ":" + to_string(i);
-          // attach callback, which itself runs a lpc on progress_persona on completion
+          // attach callback, which itself runs a LPC on progress_persona on completion
           dmap.find(key, progress_persona, 
               [key,&lpc_count](string val) {
                 assert(val == key);
                 lpc_count--;
               });
         }
-
-        //block here until the progress thread has executed all rpcs and lpcs
+        // block here until the progress thread has executed all RPCs and LPCs
         while(thread_barrier.load(memory_order_acquire) != 1){
           sched_yield();
           upcxx::progress();
@@ -86,21 +80,20 @@ int main(int argc, char *argv[])
         });
   }
 
-  //wait until all threads are done
+  // wait until all threads are done
   progress_thread.join();
   for (int tid=0; tid<num_threads; tid++) {
     threads[tid]->join();
     delete threads[tid];
   }
-
   {
-    //capture the master persona again before calling barrier and finalize
+    // push the master persona onto the initial thread's persona stack
+    // before calling barrier and finalize
     upcxx::persona_scope scope(upcxx::master_persona());
+    // wait until all processes are done
     upcxx::barrier();
-    
     if (upcxx::rank_me() == 0 )
       cout<<"SUCCESS"<<endl;
-
     upcxx::finalize();
   }
   //SNIPPET
