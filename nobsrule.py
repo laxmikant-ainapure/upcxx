306fc97ebd925336499c2d812adf406e504ed255
bonachea <dobonachea@lbl.gov>
2018-09-26 22:32:38 -0700

    Restore collaborator-snapshot GEX link
    This reverts commit f974e95fa4e760d68f98fd62fcffb8e1285ba954.

--- nobsrule.py
+++ nobsrule.py
@@ -131,7 +131,7 @@ def gasnet_user(cxt):
   value = env('GASNET', None)
   
   if not value:
-    default_gasnetex_url_b64 = 'aHR0cHM6Ly9nYXNuZXQubGJsLmdvdi9FWC9HQVNOZXQtMjAxOC45LjAudGFyLmd6'
+    default_gasnetex_url_b64 = 'aHR0cHM6Ly9tYW50aXMubGJsLmdvdi9uaWdodGx5L3VubGlzdGVkL0dBU05ldC1FWC1jb2xsYWJvcmF0b3Itc25hcHNob3QudGFyLmd6'
     import base64
     value = base64.b64decode(default_gasnetex_url_b64)
   
