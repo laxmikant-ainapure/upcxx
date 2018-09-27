1c0a8fd72f0eb307322a3a888357097d9ca2fa4f
bonachea <dobonachea@lbl.gov>
2018-09-26 22:32:24 -0700

    post-release version bump

--- src/upcxx.hpp
+++ src/upcxx.hpp
@@ -3,7 +3,7 @@
 
 // UPCXX_VERSION release identifier format:
 // YYYYMMPP = [YEAR][MONTH][PATCH]
-#define UPCXX_VERSION 20180900
+#define UPCXX_VERSION 20180901
 
 #include <upcxx/allocate.hpp>
 #include <upcxx/atomic.hpp>
