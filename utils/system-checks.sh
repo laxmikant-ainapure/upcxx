laxmikant-ainapure/upcxx

commit ef8b6639f9f3dc580561b40dfbe11f03190858a2

fixup: install: enable packaging of offline installers
Fix a Cray XC warning check that was accidentally disabled for
offline installers in 8305c96.

utils/system-checks.sh
@@ -45,7 +45,7 @@ platform_sanity_checks() {
         elif test -n "$CRAY_PRGENVGNU" || test -n "$CRAY_PRGENVINTEL" ; then
             CC=${CC:-cc}
             CXX=${CXX:-CC}
-	    if test -z "$CROSS" && test -z "$GASNET" ; then
+	    if test -z "$CROSS" ; then
 	      echo 'WARNING: To build for Cray XC compute nodes, you should set the CROSS variable (e.g. CROSS=cray-aries-slurm)'
 	    fi
         elif test "$KERNEL" = "Darwin" ; then # default to XCode clang

commit 58f9fe8ff245f9aaddca45e095192325ce7f905c

fix a CI warning
commit ef8b663 inadvertently introduced an installer warning for CI
on Cray, which uses GASNET=whatever install -single
with whatever set to values that often include a build or install tree.

Ensure we only generate the warning when nobs is being asked to
configure GASNet, which is the only time the CROSS variable is relevant.

utils/system-checks.sh
@@ -45,7 +45,8 @@ platform_sanity_checks() {
         elif test -n "$CRAY_PRGENVGNU" || test -n "$CRAY_PRGENVINTEL" ; then
             CC=${CC:-cc}
             CXX=${CXX:-CC}
-	    if test -z "$CROSS" ; then
+	    # second condition eliminates build warnings in CI for: GASNET=build_or_inst_dir install -single
+	    if [[ -z "$CROSS" && ( -z "$GASNET" || -f "$GASNET" || -f "$GASNET"/configure ) ]] ; then
 	      echo 'WARNING: To build for Cray XC compute nodes, you should set the CROSS variable (e.g. CROSS=cray-aries-slurm)'
 	    fi
         elif test "$KERNEL" = "Darwin" ; then # default to XCode clang