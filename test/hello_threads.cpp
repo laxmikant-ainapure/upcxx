d42ebda9c10dd6aecc5a710cc1ece1ae9c2ac671
bonachea <dobonachea@lbl.gov>
2018-11-13 21:22:44 -0500

    fixup: Add test for issue 171/157
    insert an I/O flush, to improve the chances of seeing initial output

--- test/hello_threads.cpp
+++ test/hello_threads.cpp
@@ -9,8 +9,8 @@ const int thread_n = 8;
 int main() {
   std::atomic<int> setup_bar{0};
   auto thread_fn = [&](int me) {
-    std::string msg = "Hello from " + std::to_string(me) + '\n';
-    std::cout << msg;
+    std::string msg = "Hello from " + std::to_string(me);
+    std::cout << msg << std::endl;
     setup_bar.fetch_add(1);
     while(setup_bar.load(std::memory_order_relaxed) != thread_n)
       sched_yield();
