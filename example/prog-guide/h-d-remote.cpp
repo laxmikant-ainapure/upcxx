58601a4bbb566b27af6af6e7442d8cd2535fa652
Mathias Jacquelin <mjacquelin@lbl.gov>
2019-03-11 14:30:32 -0400

    Remove cuda makefile and blocks around the device allocator

--- example/prog-guide/h-d-remote.cpp
+++ example/prog-guide/h-d-remote.cpp
@@ -5,59 +5,58 @@ using namespace std;
 using namespace upcxx; 
 
 #if !UPCXX_CUDA_ENABLED
-  #error "UPC++ should be build with CUDA support."
+#error "UPC++ should be build with CUDA support."
 #endif
 
 int main() {
-  upcxx::init();
+	upcxx::init();
 
-  std::size_t segsize = 256*1024*1024; // 256MB
-  auto gpu_device = upcxx::cuda_device( 0 ); // open device 0 (or other args TBD)
-  {
-    auto gpu_alloc = upcxx::device_allocator<upcxx::cuda_device>(gpu_device, segsize); // alloc GPU segment
+	std::size_t segsize = 256*1024*1024; // 256MB
+	auto gpu_device = upcxx::cuda_device( 0 ); // open device 0 (or other args TBD)
+	auto gpu_alloc = upcxx::device_allocator<upcxx::cuda_device>(gpu_device, segsize); // alloc GPU segment
 
-    // alloc an array of 1024 doubles on GPU and host
-    global_ptr<double,memory_kind::cuda_device> gpu_array = gpu_alloc.allocate<double>(1024);
-    global_ptr<double> host_array1 = upcxx::new_array<double>(1024);
-    global_ptr<double> host_array2 = upcxx::new_array<double>(1024);
+	// alloc an array of 1024 doubles on GPU and host
+	global_ptr<double,memory_kind::cuda_device> gpu_array = gpu_alloc.allocate<double>(1024);
+	global_ptr<double> host_array1 = upcxx::new_array<double>(1024);
+	global_ptr<double> host_array2 = upcxx::new_array<double>(1024);
 
 
-    double *h1 = host_array1.local();
-    double *h2 = host_array2.local();
-    //initialize h1
-    for (int i=0; i< 1024; i++)
-      h1[i] = i;
+	double *h1 = host_array1.local();
+	double *h2 = host_array2.local();
+	//initialize h1
+	for (int i=0; i< 1024; i++)
+		h1[i] = i;
 
-//SNIPPET
-    dist_object<global_ptr<double,memory_kind::cuda_device>> dobj(gpu_array);
-    int neighbor = (rank_me() + 1) % rank_n();
-    global_ptr<double,memory_kind::cuda_device> other_gpu_array = dobj.fetch(neighbor).wait();
+	//SNIPPET
+	dist_object<global_ptr<double,memory_kind::cuda_device>> dobj(gpu_array);
+	int neighbor = (rank_me() + 1) % rank_n();
+	global_ptr<double,memory_kind::cuda_device> other_gpu_array = dobj.fetch(neighbor).wait();
 
-    // copy data from host to remote GPU
-    // To move data in the opposite direction, swap the first two arguments
-    upcxx::copy(host_array1, other_gpu_array, 1024).wait();
-    upcxx::copy(other_gpu_array, host_array2, 1024).wait();
-//SNIPPET
+	// copy data from host to remote GPU
+	// To move data in the opposite direction, swap the first two arguments
+	upcxx::copy(host_array1, other_gpu_array, 1024).wait();
+	upcxx::copy(other_gpu_array, host_array2, 1024).wait();
 
-    int nerrs = 0;
-    for (int i=0; i< 1024; i++){
-      if (h1[i] != h2[i]){
-        if (nerrs < 10)
-          cout << "Error at element " << i << endl;
-        nerrs++;
-      }
-    }
-    if (nerrs)
-      cout << "Failure: " << nerrs << " errors detected\n";
-    else
-      cout << "Success" << endl;
+	upcxx::barrier();
+	//SNIPPET
 
-    upcxx::barrier();
+	int nerrs = 0;
+	for (int i=0; i< 1024; i++){
+		if (h1[i] != h2[i]){
+			if (nerrs < 10)
+				cout << "Error at element " << i << endl;
+			nerrs++;
+		}
+	}
+	if (nerrs)
+		cout << "Failure: " << nerrs << " errors detected\n";
+	else
+		cout << "Success" << endl;
 
-    gpu_alloc.deallocate(gpu_array);
-    upcxx::delete_array(host_array1);
-    upcxx::delete_array(host_array2);
-  }
-  gpu_device.destroy();
-  upcxx::finalize();
+	gpu_alloc.deallocate(gpu_array);
+	upcxx::delete_array(host_array1);
+	upcxx::delete_array(host_array2);
+
+	gpu_device.destroy();
+	upcxx::finalize();
 }
--- example/prog-guide/h-d.cpp
+++ example/prog-guide/h-d.cpp
@@ -5,7 +5,7 @@ using namespace std;
 using namespace upcxx;
 
 #if !UPCXX_CUDA_ENABLED
-  #error "UPC++ should be build with CUDA support."
+#error "UPC++ should be build with CUDA support."
 #endif
 
 int main() {
@@ -13,42 +13,41 @@ int main() {
 
   std::size_t segsize = 256*1024*1024; // 256MB
   auto gpu_device = upcxx::cuda_device( 0 ); // open device 0 (or other args TBD)
-  {
-    auto gpu_alloc = upcxx::device_allocator<upcxx::cuda_device>(gpu_device, segsize); // alloc GPU segment
+  auto gpu_alloc = upcxx::device_allocator<upcxx::cuda_device>(gpu_device, segsize); // alloc GPU segment
 
-    // alloc an array of 1024 doubles on GPU and host
-    global_ptr<double,memory_kind::cuda_device> gpu_array = gpu_alloc.allocate<double>(1024);
-    global_ptr<double> host_array1 = new_array<double>(1024);
-    global_ptr<double> host_array2 = new_array<double>(1024);
+  // alloc an array of 1024 doubles on GPU and host
+  global_ptr<double,memory_kind::cuda_device> gpu_array = gpu_alloc.allocate<double>(1024);
+  global_ptr<double> host_array1 = new_array<double>(1024);
+  global_ptr<double> host_array2 = new_array<double>(1024);
 
-    double *h1 = host_array1.local();
-    double *h2 = host_array2.local();
-    // initialize h1
-    for (int i=0; i< 1024; i++)
-      h1[i] = i;
+  double *h1 = host_array1.local();
+  double *h2 = host_array2.local();
+  // initialize h1
+  for (int i=0; i< 1024; i++)
+    h1[i] = i;
 
-    // copy data from host to GPU
-    // To move data in the opposite direction, swap the first two arguments
-    upcxx::copy(host_array1, gpu_array, 1024).wait();
-    upcxx::copy(gpu_array, host_array2, 1024).wait();
+  // copy data from host to GPU
+  // To move data in the opposite direction, swap the first two arguments
+  upcxx::copy(host_array1, gpu_array, 1024).wait();
+  upcxx::copy(gpu_array, host_array2, 1024).wait();
 
-    int nerrs = 0;
-    for (int i=0; i< 1024; i++){
-      if (h1[i] != h2[i]){
-        if (nerrs < 10)
-          cout << "Error at element " << i << endl;
-        nerrs++;
-      }
+  int nerrs = 0;
+  for (int i=0; i< 1024; i++){
+    if (h1[i] != h2[i]){
+      if (nerrs < 10)
+        cout << "Error at element " << i << endl;
+      nerrs++;
     }
-    if (nerrs)
-      cout << "Failure: " << nerrs << " errors detected\n";
-    else
-      cout << "Success" << endl;
-
-    delete_array(host_array2);
-    delete_array(host_array1);
-    gpu_alloc.deallocate(gpu_array);
   }
+  if (nerrs)
+    cout << "Failure: " << nerrs << " errors detected\n";
+  else
+    cout << "Success" << endl;
+
+  delete_array(host_array2);
+  delete_array(host_array1);
+  gpu_alloc.deallocate(gpu_array);
+
   gpu_device.destroy();
   upcxx::finalize();  // We have suppressed deallocation
 }
