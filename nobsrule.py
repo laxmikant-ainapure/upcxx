b7002046d022368f68ee2e2b3fb210af837602fd
bonachea <dobonachea@lbl.gov>
2019-03-15 18:59:51 -0400

    Restore GEX URL to C-S
    This reverts commit 346d9267cc1e5c5dbec6d7b06a57b66cff889acf.

--- nobsrule.py
+++ nobsrule.py
@@ -271,7 +271,7 @@ def gasnet_user(cxt):
   value = env('GASNET', None)
   
   if not value:
-    default_gasnetex_url_b64 = 'aHR0cHM6Ly9nYXNuZXQubGJsLmdvdi9FWC9HQVNOZXQtMjAxOS4zLjAudGFyLmd6'
+    default_gasnetex_url_b64 = 'aHR0cHM6Ly91cGMtYnVncy5sYmwuZ292L25pZ2h0bHkvdW5saXN0ZWQvR0FTTmV0LUVYLWNvbGxhYm9yYXRvci1zbmFwc2hvdC50YXIuZ3o='
     import base64
     value = base64.b64decode(default_gasnetex_url_b64)
   
