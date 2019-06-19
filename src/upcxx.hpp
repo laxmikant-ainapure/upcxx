2dce1d635af74d7d6b6c68bd0896a74dd59bb3ed
bonachea <dobonachea@lbl.gov>
2019-06-19 17:56:17 -0400

    Bump patch number to 2019.3.5

--- src/upcxx.hpp
+++ src/upcxx.hpp
@@ -3,7 +3,7 @@
 
 // UPCXX_VERSION release identifier format:
 // YYYYMMPP = [YEAR][MONTH][PATCH]
-#define UPCXX_VERSION 20190303
+#define UPCXX_VERSION 20190305
 
 #include <upcxx/allocate.hpp>
 #include <upcxx/atomic.hpp>
