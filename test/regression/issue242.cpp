baaae5f69b721e47e89dcb35454e6f94a5c8de74
bonachea <dobonachea@lbl.gov>
2019-07-26 22:45:08 -0700

    test/regression/issue242: fix a harmless warning

--- test/regression/issue242.cpp
+++ test/regression/issue242.cpp
@@ -10,9 +10,9 @@ long incoming;
 
 int main(int argc, char **argv) {
   long iters = 0;
-  long sz = 0;
+  std::size_t sz = 0;
   if (argc > 1) iters = atol(argv[1]);
-  if (argc > 2) sz = atol(argv[2]);
+  if (argc > 2) sz = (std::size_t)atol(argv[2]);
   if (iters <= 0) iters = 10000;
   if (sz <= 0) sz = 10*1024*1024;
   incoming = iters;
