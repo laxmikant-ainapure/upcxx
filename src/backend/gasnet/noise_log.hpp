bf390b17b111b6f4067944b60a528ed08585193b
John Bachan <jdbachan@lbl.gov>
2019-08-24 12:42:35 -0700

    This makes calls to `sched_yield()` from `upcxx::progress()` overridable by the user with new env var UPCXX_OVERSUBSCRIBED. Its default value is chosen the same way UPCR does it for politeness.
    Also, diagnostic "noise" generated in upxx::init/finalize have
    been overhauled for uniformity.
    
    New doc: docs/oversubscription.md

---
+++ src/backend/gasnet/noise_log.hpp
@@ -0,0 +1,63 @@
+#ifndef _1e82e1a3_aff9_4d11_8728_ae3772c6a6f7
+#define _1e82e1a3_aff9_4d11_8728_ae3772c6a6f7
+
+#include <sstream>
+
+namespace upcxx {
+namespace backend {
+namespace gasnet {
+  class noise_log {
+    const char *fn_name;
+    std::stringstream ss;
+    bool empty = true;
+    
+  public:
+    noise_log(const char *fn_name = nullptr) {
+      this->fn_name = fn_name;
+    }
+    
+    struct line_type {
+      noise_log *parent;
+
+      line_type(noise_log *parent) {
+        this->parent = parent;
+      }
+      line_type(line_type &&that) {
+        this->parent = that.parent;
+        that.parent = nullptr;
+      }
+      ~line_type() {
+        if(parent)
+          parent->ss << '\n';
+      }
+      
+      template<typename T>
+      line_type& operator<<(T &&x) {
+        parent->ss << std::forward<T>(x);
+        return *this;
+      }
+
+      line_type& operator<<(std::ostream&(&op)(std::ostream&)) {
+        op(parent->ss);
+        return *this;
+      }
+      line_type& operator<<(std::ios_base&(&op)(std::ios_base&)) {
+        op(parent->ss);
+        return *this;
+      }
+    };
+
+    line_type line() {
+      empty = false;
+      ss << "> ";
+      return line_type{this};
+    }
+
+    line_type warn() {
+      return static_cast<line_type&&>(line() << "WARNING: ");
+    }
+
+    void show();
+  };
+}}}
+#endif
