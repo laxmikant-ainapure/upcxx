8343b705ac013ade2c811f6bb4b1398c00311ee5
bonachea <dobonachea@lbl.gov>
2020-01-05 03:53:57 -0500

    Bump patch number

--- src/upcxx.hpp
+++ src/upcxx.hpp
@@ -3,7 +3,7 @@
 
 // UPCXX_VERSION release identifier format:
 // YYYYMMPPL = [YEAR][MONTH][PATCH]
-#define UPCXX_VERSION 20190905L
+#define UPCXX_VERSION 20190907L
 
 // UPCXX_SPEC_VERSION release identifier format:
 // YYYYMM00L = [YEAR][MONTH]
