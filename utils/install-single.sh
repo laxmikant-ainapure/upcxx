6fd95fd72c2b26bfa3cd5979e938eae59d8ad94d
Paul H. Hargrove <PHHargrove@lbl.gov>
2020-01-09 01:27:33 -0800

    Add `utils/install-single.sh` and remove `install`
    This commit extracts the `-single` logic from the top-level `install`,
    to create `utils/install-single.sh`.  The top-level `install` is then
    removed.  The src-vs-bld logic has moved into the main Makefile.

---
+++ utils/install-single.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$#" -eq 1 ]; then
+  prefix="$1"
+else
+  echo usage: $(basename $0) INSTALL_DIR >&2
+  exit 1
+fi
+
+# Configure:
+./configure --prefix="${prefix}" --single || exit $?
+
+# Extract:
+GMAKE=$(grep '^export GMAKE ' Makefile | tr -s ' ' | cut -d' ' -f4) || exit $?
+
+# Install:
+${UPCXX_MAKE:-$GMAKE -j8} install-single
+if [[ $? -ne 0 ]]; then
+  echo 'UPC++ install failed'
+  exit 1;
+fi
+echo 'UPC++ successfully installed'
