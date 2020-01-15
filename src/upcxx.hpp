79bb89e3cb3fa75f0359ed5b3feb84cc51274924
Paul H. Hargrove <PHHargrove@lbl.gov>
2020-01-15 15:41:35 -0800

    Advance version markings to 2019.9.5 for development

--- src/upcxx.hpp
+++ src/upcxx.hpp
@@ -3,7 +3,7 @@
 
 // UPCXX_VERSION release identifier format:
 // YYYYMMPPL = [YEAR][MONTH][PATCH]
-#define UPCXX_VERSION 20190904L
+#define UPCXX_VERSION 20190905L
 
 // UPCXX_SPEC_VERSION release identifier format:
 // YYYYMM00L = [YEAR][MONTH]
