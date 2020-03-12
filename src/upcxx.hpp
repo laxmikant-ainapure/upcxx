a4933d24378199c51978b1cb980e55e705eec967
bonachea <dobonachea@lbl.gov>
2020-03-11 23:30:09 -0400

    2020.3.0 package version bumps

--- src/upcxx.hpp
+++ src/upcxx.hpp
@@ -3,11 +3,11 @@
 
 // UPCXX_VERSION release identifier format:
 // YYYYMMPPL = [YEAR][MONTH][PATCH]
-#define UPCXX_VERSION 20190907L
+#define UPCXX_VERSION 20200300L
 
 // UPCXX_SPEC_VERSION release identifier format:
 // YYYYMM00L = [YEAR][MONTH]
-#define UPCXX_SPEC_VERSION 20190900L
+#define UPCXX_SPEC_VERSION 20200300L
 
 namespace upcxx {
   long release_version();
