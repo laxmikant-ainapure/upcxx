5f60257982974ead8aebfc1a0d0a9d9c221e2634
Amir Kamil <akamil@umich.edu>
2020-08-13 13:16:38 -0400

    Add to issue402 test case.

--- test/regression/issue402.cpp
+++ test/regression/issue402.cpp
@@ -2,9 +2,16 @@
 
 #include "../util.hpp"
 
+struct B {
+  int x;
+  B(int v) : x(v) {}
+  B(const B&) = delete; // non-copyable, non-movable
+};
+
 struct A {
   int x;
   A(int v) : x(v) {}
+  A(const B& b) : x(b.x) {}
   A(const A&) = delete; // non-copyable, non-movable
 };
 
@@ -12,15 +19,43 @@ int main() {
   upcxx::init();
   print_test_header();
 
-  upcxx::promise<A> pro1;
-  pro1.fulfill_result(3);
-  UPCXX_ASSERT_ALWAYS(pro1.get_future().wait_reference().x == 3);
+  {
+    upcxx::promise<A> pro;
+    pro.fulfill_result(3);
+    UPCXX_ASSERT_ALWAYS(pro.get_future().wait_reference().x == 3);
+  }
+  {
+    upcxx::promise<A, A> pro;
+    pro.fulfill_result(4, 5);
+    UPCXX_ASSERT_ALWAYS(pro.get_future().wait_reference<0>().x == 4);
+    UPCXX_ASSERT_ALWAYS(pro.get_future().wait_reference<1>().x == 5);
+  }
 
-  upcxx::promise<A, A> pro2;
   // undocumented fulfill_result with tuple
-  pro2.fulfill_result(std::make_tuple(-1, -2));
-  UPCXX_ASSERT_ALWAYS(pro2.get_future().wait_reference<0>().x == -1);
-  UPCXX_ASSERT_ALWAYS(pro2.get_future().wait_reference<1>().x == -2);
+  {
+    upcxx::promise<A> pro;
+    pro.fulfill_result(std::make_tuple(-3));
+    UPCXX_ASSERT_ALWAYS(pro.get_future().wait_reference().x == -3);
+  }
+  {
+    upcxx::promise<A, A> pro;
+    pro.fulfill_result(std::make_tuple(-1, -2));
+    UPCXX_ASSERT_ALWAYS(pro.get_future().wait_reference<0>().x == -1);
+    UPCXX_ASSERT_ALWAYS(pro.get_future().wait_reference<1>().x == -2);
+  }
+
+  // construct non-movable A from non-movable B
+  {
+    upcxx::promise<A> pro;
+    pro.fulfill_result(B(3));
+    UPCXX_ASSERT_ALWAYS(pro.get_future().wait_reference().x == 3);
+  }
+  {
+    upcxx::promise<A, A> pro;
+    pro.fulfill_result(B(4), B(5));
+    UPCXX_ASSERT_ALWAYS(pro.get_future().wait_reference<0>().x == 4);
+    UPCXX_ASSERT_ALWAYS(pro.get_future().wait_reference<1>().x == 5);
+  }
 
   print_test_success();
   upcxx::finalize();
