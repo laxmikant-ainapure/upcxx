aa298797f2727bfb122adc9245fe6fb1280cc2cb
Amir Kamil <akamil@umich.edu>
2020-08-16 17:43:55 -0400

    Cherry pick test cases from PR #257.

--- test/rpc-ctor-trace.cpp
+++ test/rpc-ctor-trace.cpp
@@ -43,9 +43,13 @@ void T::show_stats(const char *title, int expected_ctors, int expected_copies) {
   upcxx::barrier();
 }
 
+T global;
+
 int main() {
   upcxx::init();
   print_test_header();
+
+  --T::ctors; // discount construction of global
 
   using upcxx::dist_object;
   dist_object<int> dob(3);
@@ -155,6 +159,26 @@ int main() {
     T()
   ).wait_reference();
   T::show_stats("T const& -> future<T const&>", 3, 0);
+
+  upcxx::rpc(target,
+    [](upcxx::view<T> v) -> T const& {
+      auto storage =
+        new typename std::aligned_storage <sizeof(T),
+                                           alignof(T)>::type;
+      T *p = v.begin().deserialize_into(storage);
+      delete p;
+      return global;
+    },
+    upcxx::make_view(&global, &global+1)
+  ).wait_reference();
+  T::show_stats("view<T> -> T const&", 2, 0);
+
+  upcxx::rpc(target,
+    []() -> T const& {
+      return global;
+    }
+  ).wait_reference();
+  T::show_stats("-> T const&", 1, 0);
 
   print_test_success();
   upcxx::finalize();
