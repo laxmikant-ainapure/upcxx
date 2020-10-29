b5fedf2e803ab579acb164cd1c7e102b5d089a2c
bonachea <dobonachea@lbl.gov>
2020-10-29 13:35:06 -0700

    syncx3 updated examples from guide
    (cherry picked from commit 59d2b3e6a25213e1256733d475b0f57b6b9113db)

--- example/prog-guide/view-matrix-tasks.hpp
+++ example/prog-guide/view-matrix-tasks.hpp
@@ -3,28 +3,22 @@
 #include <vector>
 
 //SNIPPET
-// Number of worker threads/personas.
-constexpr int worker_n = 8;
+double my_matrix[1000][1000] = {/*0...*/}; // Rank's local matrix.
+constexpr int worker_n = 8; // Number of worker threads/personas.
 
 // Each persona has a dedicated thread spinning on its progress engine.
 upcxx::persona workers[worker_n];
-
-// Rank's local matrix.
-double my_matrix[1000][1000] = {/*0...*/};
 
 struct element {
   int row, col;
   double value;
 };
 
-upcxx::future<> update_remote_matrix(
-    upcxx::intrank_t rank,
-    element const *elts, int elt_n
-  ) {
+upcxx::future<> update_remote_matrix(upcxx::intrank_t rank,
+                                     element const *elts, int elt_n) {
   return upcxx::rpc(rank,
     [](upcxx::view<element> const &elts_in_rpc) {
       upcxx::future<> all_done = upcxx::make_future();
-      
       for(int w=0; w < worker_n; w++) {
         // Launch task on respective worker.
         auto task_done = workers[w].lpc(
@@ -37,11 +31,9 @@ upcxx::future<> update_remote_matrix(
             }
           }
         );
-        
         // Conjoin task completion into `all_done`.
         all_done = upcxx::when_all(all_done, task_done);
       }
-      
       // Returned future has a dependency on each task lpc so the network
       // buffer (thus `elts_in_rpc` view) will remain valid until all tasks
       // have completed.
