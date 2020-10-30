065b1379d517761756e9c3369e9bfb2ba97fe7a0
Paul H. Hargrove <PHHargrove@lbl.gov>
2020-10-29 17:36:11 -0700

    Bump development version to 2020.10.1

--- src/version.hpp
+++ src/version.hpp
@@ -3,7 +3,7 @@
 
 // UPCXX_VERSION release identifier format:
 // YYYYMMPPL = [YEAR][MONTH][PATCH]
-#define UPCXX_VERSION 20201000L
+#define UPCXX_VERSION 20201001L
 
 // UPCXX_SPEC_VERSION release identifier format:
 // YYYYMM00L = [YEAR][MONTH]
