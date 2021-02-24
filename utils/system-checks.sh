570c5c78e99db0dfdb7b03be456ea911c6dc56d0
Paul H. Hargrove <PHHargrove@lbl.gov>
2021-02-23 20:13:54 -0800

    configure: recognize new Intel compilers
    This commit modifies the regexp used to test Intel compiler versions
    to accept the full (4-digits) years which are used in the current
    release:
    
    OLD:
    ```
    $ icc --version | head -1
    icc (ICC) 19.0.3.199 20190206
    ```
    
    NEW:
    ```
    $ icc --version | head -1
    icc (ICC) 2021.1.2 20201208
    ```

--- utils/system-checks.sh
+++ utils/system-checks.sh
@@ -325,12 +325,12 @@ platform_sanity_checks() {
              ! check_gnu_version CXX &> /dev/null; then
             COMPILER_BAD=1
         elif test -z "$CRAY_PRGENVINTEL" && \
-             echo "$CXXVERS" | egrep ' +\(ICC\) +(17\.0\.[2-9]|1[89]\.|2[0-9]\.)' 2>&1 > /dev/null ; then
+             echo "$CXXVERS" | egrep ' +\(ICC\) +(17\.0\.[2-9]|1[89]\.|(20)?2[0-9]\.)' 2>&1 > /dev/null ; then
 	    # Ex: icpc (ICC) 18.0.1 20171018
             check_intel_compiler || exit 1
             COMPILER_GOOD=1
         elif test -n "$CRAY_PRGENVINTEL" && \
-             echo "$CXXVERS" | egrep ' +\(ICC\) +(18\.0\.[1-9]|19\.|2[0-9]\.)' 2>&1 > /dev/null ; then
+             echo "$CXXVERS" | egrep ' +\(ICC\) +(18\.0\.[1-9]|19\.|(20)?2[0-9]\.)' 2>&1 > /dev/null ; then
             check_intel_compiler || exit 1
             COMPILER_GOOD=1
         elif echo "$CXXVERS" | egrep ' +\(ICC\) ' 2>&1 > /dev/null ; then
